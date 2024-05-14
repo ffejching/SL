feb977da11fe06cffa86d381d7aaf59f6ced7286
ffejching <165637526+ffejching@users.noreply.github.com>
2024-05-14 04:20:18 +0000

    HelloWorld

---
+++ Task121/Task111/HelloWorld.java
@@ -0,0 +1,11 @@
+package Task121.Task111;
+
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
