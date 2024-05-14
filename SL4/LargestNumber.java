310fc381fabf621bdb870ea57f3a839ae4b11b91
ffejching <165637526+ffejching@users.noreply.github.com>
2024-05-14 05:18:41 +0000

    LargestNumber

--- SL4/LargestNumber.java
+++ SL4/LargestNumber.java
@@ -5,11 +5,25 @@ import java.util.Scanner;
 public class LargestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.println("Enter three numbers:");
-        double num1 = scanner.nextDouble();
-        double num2 = scanner.nextDouble();
-        double num3 = scanner.nextDouble();
-
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        
         if (num1 == num2 && num2 == num3) {
-        }}}
+            System.out.println("All numbers are equal");
+        } else {
+            int largest = num1;
+            if (num2 > largest) {
+                largest = num2;
+            }
+            if (num3 > largest) {
+                largest = num3;
+            }
+            System.out.println("The largest number is: " + largest);
+        }
+        
+        scanner.close();
+    }
+}
