28cbf4be09073c24089c825e934cfc7e7f2831f8
ffejching <165637526+ffejching@users.noreply.github.com>
2024-05-14 01:50:47 +0000

    ArithmeticTask

---
+++ SL4/LargestNumber.java
@@ -0,0 +1,15 @@
+package SL4;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three numbers:");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        double num3 = scanner.nextDouble();
+
+        if (num1 == num2 && num2 == num3) {
+        }}}
